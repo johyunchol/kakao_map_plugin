524e3a3afb1a9964eed32c402dada076c50d71a2
kkensu <kkensu@gmail.com>
2024-10-06 15:02:31 +0900

    Updated version 0.3.2

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/johyunchol/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/johyunchol/Documents/workspace/kkensu/kakao_map_plugin/example"
+export "FLUTTER_ROOT=/Users/kkensu/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kkensu/Documents/workspace/kkensu/kakao_map_plugin/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
